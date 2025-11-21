c66d470d761f7c4ccebb797520d776772fab9f53
Mohankumar-Eng <mk.undamanti@gmail.com>
2025-11-21 18:49:42 +0530

    add data

--- 04-arguments.sh
+++ 04-arguments.sh
@@ -3,7 +3,7 @@
 person=$1
 greeting=$2
 
-echo "$greeting, $$person!"
+echo "$greeting, $person!"
 echo "How are you, $person?"
 echo "Your first argument is: $1"
 echo "Your second argument is: $2"
