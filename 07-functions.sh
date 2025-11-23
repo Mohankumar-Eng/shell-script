4210106b0c0959f50750074bce2b2d0e1a4c2429
Mohankumar-Eng <mk.undamanti@gmail.com>
2025-11-23 17:04:32 +0530

    add functions

---
+++ 07-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then 
+        echo "Installing....$2 FALIED"
+    else
+        echo "Installing......$2 SUCCESS"
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: please run this script with root access"
+else
+    echo "your are the root"
+fi
+
+yum install mysql -y
+VALIDATE $? "MySQL"
+yum install git -y
+VALIDATE $? "GIT"
