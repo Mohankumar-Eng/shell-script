45efb7c810b75f797d8859e79cf410604a51907f
Mohankumar-Eng <mk.undamanti@gmail.com>
2025-11-23 18:10:29 +0530

    add functions

--- 08-log.sh
+++ 08-log.sh
@@ -24,7 +24,7 @@ else
     echo "you are the root"
 fi
 
-yum install mysql -y &>> $Logfile
+yum install mysqll -y &>> $Logfile
 VALIDATE $? "MYSQL" 
 yum install git -y &>> $Logfile
 VALIDATE $? "GIT" 
