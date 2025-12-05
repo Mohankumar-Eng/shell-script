57b261dc62ff17424f4fb2c39f23b0c4f26640c5
Mohankumar-Eng <mk.undamanti@gmail.com>
2025-12-05 23:48:30 +0530

    add some data

--- 12-disk-usage.sh
+++ 12-disk-usage.sh
@@ -3,13 +3,13 @@
 disk_usage=$(df -hT | grep -vE "tmp/file")
 
 disk_threshold=1
-mes=""
+mes=" "
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-while IFS=read line
+while IFS= read line
 do
     usage=$(echo $line | awk '{print $6f}' | cut -d % f1)
     partition=$(echo $line | awk '{print $6f}')
