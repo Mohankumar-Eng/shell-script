5875b148181a8f3b49bca7803b3bca97b2b4bb0e
Mohankumar-Eng <mk.undamanti@gmail.com>
2025-12-05 23:47:16 +0530

    add some data

---
+++ 12-disk-usage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+disk_usage=$(df -hT | grep -vE "tmp/file")
+
+disk_threshold=1
+mes=""
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+while IFS=rad line
+do
+    usage=$(echo $line | awk '{print $6f}' | cut -d % f1)
+    partition=$(echo $line | awk '{print $6f}')
+    if [ $usage -ge $disk_threshold ]
+    then
+        mes+="High disk usage on $partition: $usage\n"
+    fi
+done <<<$disk_usage
+echo -e "message:$mes"
