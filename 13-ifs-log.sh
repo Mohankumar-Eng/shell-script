b43f739d1656c4e5f866c4664747f3829d20f705
Mohankumar-Eng <mk.undamanti@gmail.com>
2025-12-05 23:34:52 +0530

    add some data

---
+++ 13-ifs-log.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+file=/etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file ]
+then
+    echo -e "$R source filr : $file does not exit $N"
+    exit 1
+fi
+
+while IFS= ":" read -r user_name password user_id group_id user_fullname home_dir shell_path
+do
+    echo "user name:$user_name"
+    echo "user id:$user_id"
+    echo "user_fullname: $user_fullname"
+done < f$file
