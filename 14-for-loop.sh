2f232bdeb7165410bed6fb2a6a73da470f56ff70
Mohankumar-Eng <mk.undamanti@gmail.com>
2026-02-08 11:38:29 +0530

    loops

--- 14-for-loop.sh
+++ 14-for-loop.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "\e[31m ERROR: : You must be run this script as root user \e[0m"
+    echo -e "$R ERROR: : You must be run this script as root user $N"
     exit 1
 else
-    echo -e "\e[32m You are the root user, you can run this script \e[0m"
+    echo -e "$G You are the root user, you can run this script $N"
 fi
 
+VALIDATE() {
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 is installed successfully $N"
+    else
+        echo -e "$R ERROR: $2 is not installed $N"
+    fi
+}
+
+for package in $@
+do
+    dnf list installed $package
+    if [ $? -eq 0 ]
+    then
+        echo "$package is not installed...going to install it"
+        dnf install $package -y
+        VALIDATE $? "$package"
+    else
+        echo "$package is already installed"
+    fi
+done
